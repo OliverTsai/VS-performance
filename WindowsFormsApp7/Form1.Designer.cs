fb2ba4375bf2084ba85a9f7c8f4d66f62d37a78f
sun <sun@shiauyau.com.tw>
2022-03-18 17:28:11 +0800

    add C#

---
+++ WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class MonitoringTools
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MonitoringTools));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.helloTab = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.memoryChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.cpuChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.helloTab.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.memoryChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpuChart)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // helloTab
+            // 
+            this.helloTab.Controls.Add(this.tabPage1);
+            this.helloTab.Controls.Add(this.tabPage2);
+            this.helloTab.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Bold);
+            this.helloTab.ItemSize = new System.Drawing.Size(315, 40);
+            this.helloTab.Location = new System.Drawing.Point(12, 49);
+            this.helloTab.Name = "helloTab";
+            this.helloTab.Padding = new System.Drawing.Point(82, 5);
+            this.helloTab.SelectedIndex = 0;
+            this.helloTab.Size = new System.Drawing.Size(664, 719);
+            this.helloTab.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Black;
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.memoryChart);
+            this.tabPage1.Controls.Add(this.cpuChart);
+            this.tabPage1.Controls.Add(this.chart1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 44);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(656, 671);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "SYSTEM INDEX 系統指標";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(276, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "            CPU\r\nxxxxx |(CPU Name)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(110, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "RAM\r\nxxx GB";
+            // 
+            // memoryChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.memoryChart.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.memoryChart.Legends.Add(legend1);
+            this.memoryChart.Location = new System.Drawing.Point(36, 122);
+            this.memoryChart.Name = "memoryChart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series2";
+            this.memoryChart.Series.Add(series1);
+            this.memoryChart.Size = new System.Drawing.Size(177, 126);
+            this.memoryChart.TabIndex = 2;
+            this.memoryChart.Text = "chart2";
+            // 
+            // cpuChart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.cpuChart.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.cpuChart.Legends.Add(legend2);
+            this.cpuChart.Location = new System.Drawing.Point(241, 122);
+            this.cpuChart.Name = "cpuChart";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.cpuChart.Series.Add(series2);
+            this.cpuChart.Size = new System.Drawing.Size(177, 126);
+            this.cpuChart.TabIndex = 1;
+            this.cpuChart.Text = "chart2";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Black;
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 44);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(656, 671);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "OFFICE IT SUPPORT 其他產品";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-4, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(664, 675);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "00000001A";
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Image = global::WindowsFormsApp7.Properties.Resources.logo;
+            this.pictureBox10.Location = new System.Drawing.Point(225, 3);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(176, 41);
+            this.pictureBox10.TabIndex = 2;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Image = global::WindowsFormsApp7.Properties.Resources.phone;
+            this.pictureBox11.Location = new System.Drawing.Point(411, 3);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(159, 54);
+            this.pictureBox11.TabIndex = 3;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.Click += new System.EventHandler(this.pictureBox11_Click);
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Image = global::WindowsFormsApp7.Properties.Resources.connect;
+            this.pictureBox12.Location = new System.Drawing.Point(508, 3);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(151, 54);
+            this.pictureBox12.TabIndex = 4;
+            this.pictureBox12.TabStop = false;
+            this.pictureBox12.Click += new System.EventHandler(this.pictureBox12_Click);
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Enabled = false;
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(452, 122);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(179, 126);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // MonitoringTools
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(688, 780);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.helloTab);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox12);
+            this.Name = "MonitoringTools";
+            this.Text = "Monitoring Tools";
+            this.helloTab.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.memoryChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpuChart)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl helloTab;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart cpuChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart memoryChart;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ WindowsFormsApp7/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+namespace WindowsFormsApp7
+{
+    public partial class MonitoringTools : Form
+    {
+        private Thread cpuThread;
+        private Thread memoryThread;
+        private double[] cpuArray = new double[60];
+        private double[] memoryArray = new double[60];
+        public MonitoringTools()
+        {
+            InitializeComponent();
+
+            this.helloTab.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.helloTab.DrawItem += new DrawItemEventHandler(this.helloTab_DrawItem);
+
+            chart1.Titles.Add("CLOUDFLARE");
+            chart1.Series["Series1"].Points.AddXY("1", "10");
+            chart1.Series["Series1"].Points.AddXY("2", "20");
+            chart1.Series["Series1"].Points.AddXY("3", "30");
+
+            cpuThread = new Thread(new ThreadStart(this.getPerformanceCounters));
+            cpuThread.IsBackground = true;
+            cpuThread.Start();
+
+            memoryThread = new Thread(new ThreadStart(this.getPerformanceCounters));
+            memoryThread.IsBackground = true;
+            memoryThread.Start();
+        }
+
+        private void helloTab_DrawItem(object sender, System.Windows.Forms.DrawItemEventArgs e)
+        {
+            Console.WriteLine("helloTab_DrawItem ENTER");
+
+            TabPage CurrentTab = helloTab.TabPages[e.Index];
+            Rectangle ItemRect = helloTab.GetTabRect(e.Index);
+            SolidBrush FillBrush = new SolidBrush(Color.White);
+            SolidBrush TextBrush = new SolidBrush(Color.FromArgb(255, 102, 0));
+            StringFormat sf = new StringFormat();
+            sf.Alignment = StringAlignment.Center;
+            sf.LineAlignment = StringAlignment.Center;
+
+            //If we are currently painting the Selected TabItem we'll
+            //change the brush colors and inflate the rectangle.
+            if (System.Convert.ToBoolean(e.State & DrawItemState.Selected))
+            {
+                FillBrush.Color = Color.FromArgb(255,102,0);
+                TextBrush.Color = Color.White;
+                ItemRect.Inflate(2, 2);
+            }
+
+            //Set up rotation for left and right aligned tabs
+            if (helloTab.Alignment == TabAlignment.Left || helloTab.Alignment == TabAlignment.Right)
+            {
+                float RotateAngle = 90;
+                if (helloTab.Alignment == TabAlignment.Left)
+                    RotateAngle = 270;
+                PointF cp = new PointF(ItemRect.Left + (ItemRect.Width / 2), ItemRect.Top + (ItemRect.Height / 2));
+                e.Graphics.TranslateTransform(cp.X, cp.Y);
+                e.Graphics.RotateTransform(RotateAngle);
+                ItemRect = new Rectangle(-(ItemRect.Height / 2), -(ItemRect.Width / 2), ItemRect.Height, ItemRect.Width);
+            }
+
+            //Next we'll paint the TabItem with our Fill Brush
+            e.Graphics.FillRectangle(FillBrush, ItemRect);
+
+            //Now draw the text.
+            e.Graphics.DrawString(CurrentTab.Text, e.Font, TextBrush, (RectangleF)ItemRect, sf);
+
+            //Reset any Graphics rotation
+            e.Graphics.ResetTransform();
+
+            //Finally, we should Dispose of our brushes.
+            FillBrush.Dispose();
+            TextBrush.Dispose();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("CCTV");
+        }
+
+        private void pictureBox11_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Call Whatsapp ...");
+        }
+
+        private void pictureBox12_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Call Remote Tools");
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("CCTV");
+        }
+        private void getPerformanceCounters()
+        {
+            var cpuPerfCounter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+            var memoryPerfCounter = new PerformanceCounter("Memory", "% Committed Bytes in Use");
+
+            while (true)
+            {
+                cpuArray[cpuArray.Length - 1] = Math.Round(cpuPerfCounter.NextValue(), 0);
+                memoryArray[memoryArray.Length - 1] = Math.Round(memoryPerfCounter.NextValue(), 0);
+
+                Array.Copy(cpuArray, 1, cpuArray, 0, cpuArray.Length - 1);
+                Array.Copy(memoryArray, 1,memoryArray, 0, memoryArray.Length -1);
+
+                if (cpuChart.IsHandleCreated)
+                {
+                    this.Invoke((MethodInvoker)delegate { UpdateCpuChart(); });
+                }
+                else
+                {
+                    //......
+                }
+
+                if (memoryChart.IsHandleCreated)
+                {
+                    this.Invoke((MethodInvoker)delegate { UpdateMemoryChart(); });
+                }
+                else
+                {
+                    //......
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void UpdateCpuChart()
+        {
+            cpuChart.Series["Series1"].Points.Clear();
+
+            for (int i = 0; i < cpuArray.Length - 1; ++i)
+            {
+                cpuChart.Series["Series1"].Points.AddY(cpuArray[i]);
+            }
+        }
+        private void UpdateMemoryChart()
+        {
+            memoryChart.Series["Series2"].Points.Clear();
+
+            for(int i = 0; i <= memoryArray.Length - 1; i++)
+            {
+                memoryChart.Series["Series2"].Points.AddY(memoryArray[i]);
+            }
+        }
+    }
+}
